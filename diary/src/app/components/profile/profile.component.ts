ae71146d2f155af33ac4eaa7c5fbb921976b3c17
sujeewakabeysinghe@gmail.com <sujeewakabeysinghe@gmail.com>
2020-05-11 14:16:10 +0530

    11/5/2020

--- diary/src/app/components/profile/profile.component.ts
+++ diary/src/app/components/profile/profile.component.ts
@@ -11,7 +11,7 @@ import { Router } from '@angular/router';
 export class ProfileComponent implements OnInit {
 
   iconname=true;
-  pickdate:string;
+  search:string;
   show:string;
   userId:string;
   userName:string;
