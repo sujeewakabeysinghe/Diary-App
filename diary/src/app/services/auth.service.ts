e727af33182f1b332f7ef52fdd32ae7583e47f6d
sujeewakabeysinghe@gmail.com <sujeewakabeysinghe@gmail.com>
2020-05-15 20:07:41 +0530

    15/5/2020

--- diary/src/app/services/auth.service.ts
+++ diary/src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http,Headers } from '@angular/http';
-import { map } from 'rxjs/operators';
+import { map } from 'rxjs/operators'; //npm i rxjs-compat
 import { tokenNotExpired } from 'angular2-jwt'; //npm install angular2-jwt
 
 @Injectable({
